8ae0a95b2d46929be21690995b7b69727408296e
Matthieu Auger <matthieu.auger@acseo-conseil.fr>
2013-05-31 15:29:49 +0200

    Allow to set specific parameters per instance

--- Service/Html2pdf.php
+++ Service/Html2pdf.php
@@ -5,14 +5,29 @@ class Html2pdf
 {
     protected $html2pdf;
 
+    public $mode;
+    public $format;
+    public $lang;
+    public $unicode;
+    public $margin;
+
     public function __construct($mode, $format, $lang,$unicode,$encoding,$margin)
     {
-        $this->html2pdf = new \HTML2PDF($mode, $format, $lang,$unicode,$encoding,$margin);
+    	$this->mode = $mode;
+    	$this->format = $format;
+    	$this->lang = $lang;
+    	$this->unicode = $unicode;
+    	$this->margin = $margin;
     }
 
-    public function get(){
+    public function init()
+    {
+    	$this->html2pdf = new \HTML2PDF($this->mode, $this->format, $this->lang, $this->unicode, $this->encoding, $this->margin);
+    }
+
+    public function get()
+    {
         return $this->html2pdf;
     }
-
 }
 
